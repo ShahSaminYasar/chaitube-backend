82538346b57393d5083ff5413028b1b764ac4c94
ShahSaminYasar <shahsaminyasarraihan@gmail.com>
2025-12-20 09:08:39 +0600

    Done (13): Logic Building - Added register account and bug fixes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,71 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { User } from "../models/user.model.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 
 const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    message: "CHAI AUR CODE",
+  const { username, email, fullname, password } = req.body;
+
+  if (
+    [fullname, email, username, password].some((field) => field?.trim() === "")
+  ) {
+    throw new ApiError(400, "All fields are required");
+  }
+
+  const existingUser = await User.findOne({
+    $or: [{ username }, { email }],
   });
+
+  if (existingUser) {
+    throw new ApiError(409, "Account with email or username already exists");
+  }
+
+  const avatarLocalPath = req.files?.avatar?.[0]?.path;
+  const coverImageLocalPath = req.files?.coverImage?.[0]?.path;
+
+  if (!avatarLocalPath) {
+    throw new ApiError(400, "Avatar image is required");
+  }
+
+  const avatar = await uploadOnCloudinary(avatarLocalPath);
+
+  if (!avatar) {
+    throw new ApiError(400, "Avatar image upload failed");
+  }
+
+  const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+
+  const user = await User.create({
+    fullname,
+    avatar: avatar?.url,
+    coverImage: coverImage?.url || "",
+    email,
+    password,
+    username: username.toLowerCase(),
+  });
+
+  const createdUser = await User.findById(user?._id).select(
+    "-password -refreshToken"
+  );
+
+  if (!createdUser) {
+    throw new ApiError(500, "Something went wrong while registering the user");
+  }
+
+  return res
+    .status(201)
+    .json(new ApiResponse(200, createdUser, "User registered successfully"));
+
+  // Get user details from frontend
+  // Validation
+  // Check if user already exists: username, email
+  // Check for images, check for avatar
+  // Upload images to Cloudinary
+  // Create user object - create entry in DB
+  // Remove password and refresh token field from response
+  // Check for user creation
+  //   Return res
 });
 
 export { registerUser };
